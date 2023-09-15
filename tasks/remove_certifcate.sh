7cf00aa0b08b610bf98bead6dad9d6a00308eae1
Chris Allen <chris.allen@puppet.com>
2023-09-15 11:56:18 +0100

    Returned correct exit code

--- tasks/remove_certifcate.sh
+++ tasks/remove_certifcate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 puppet node clean $PT_agent_certnames;
-if [ $PT_purge_data ]; then
+$status = $?
+if [ $PT_purge_data ] && [ $status == 0 ]; then
     puppet node purge $PT_agent_certnames;
+    $status = $?
 fi
-exit 0
+exit $status
